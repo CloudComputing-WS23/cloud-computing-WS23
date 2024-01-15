40ef9675b1c77cc5bbaedf7aa996c94f553aacfb
Andreas Leeb <leeb.andreas00@gmail.com>
2024-01-15 15:20:27 +0100

    opensearch part 1

--- start.bat
+++ start.bat
@@ -1,13 +1,12 @@
 @echo off
-minikube start --no-vtx-check --profile bookshop
-minikube addons enable ingress --profile bookshop
+REM prerequisites minikube
+minikube start
+minikube addons enable ingress
 kubectl apply -f .\bookshop-deployment\kubernetes\platform\development\services\postgresql.yml
 kubectl apply -f .\bookshop-deployment\kubernetes\platform\development\services\redis.yml
 kubectl apply -f .\bookshop-deployment\kubernetes\platform\development\services\rabbitmq.yml
 kubectl apply -f .\catalog-service\k8s\deployment.yml
 kubectl apply -f .\catalog-service\k8s\service.yml
-kubectl apply -f .\config-service\k8s\deployment.yml
-kubectl apply -f .\config-service\k8s\service.yml
 kubectl apply -f .\dispatcher-service\k8s\deployment.yml
 kubectl apply -f .\dispatcher-service\k8s\service.yml
 kubectl apply -f .\edge-service\k8s\deployment.yml
@@ -15,3 +14,8 @@ kubectl apply -f .\edge-service\k8s\service.yml
 kubectl apply -f .\edge-service\k8s\ingress.yml
 kubectl apply -f .\order-service\k8s\deployment.yml
 kubectl apply -f .\order-service\k8s\service.yml
+
+
+REM prerequisites: Helm
+helm repo add opensearch-operator https://opensearch-project.github.io/opensearch-k8s-operator/
+helm install opensearch-operator opensearch-operator/opensearch-operator
